631978abd5909ee282985f018771409d707ef76d
ngxuanth <ngxuanth.work@gmail.com>
2025-03-26 15:17:07 +0700

    initial

---
+++ src/electron/main.ts
@@ -0,0 +1,12 @@
+import { app, BrowserWindow } from "electron"
+import path from 'path'
+import { isDev } from "./utils.js"
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({})
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:3000')
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), "/dist/index.html"))
+    }
+})
